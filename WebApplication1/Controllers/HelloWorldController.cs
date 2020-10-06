26ee892714e97b11eac62d37d5da6a223dae13e1
Derek <dbacy@my.edgetechacademy.edu>
2020-10-06 12:36:20 -0500

    Add project files.

---
+++ WebApplication1/Controllers/HelloWorldController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult ThisIsMe()
+        {
+            ViewBag.Now = DateTime.Now;
+            ViewBag.Name = "Derek Bacy";
+            ViewBag.Hobby = "Programming";
+            return View();
+        }
+    }
+    
+}
